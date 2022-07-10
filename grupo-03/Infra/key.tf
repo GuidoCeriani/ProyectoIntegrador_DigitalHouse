f218f29f2fad38e3b2503717c8681aeae6b69e6c
Guido <gmceriani888@hotmail.com>
2022-07-10 19:37:37 -0300

    Proyecto Integrador

--- grupo-03/Infra/key.tf
+++
@@ -1,9 +0,0 @@
-resource "tls_private_key" "rsa-4096" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "c1g3-key"
-  public_key = tls_private_key.rsa-4096.public_key_openssh
-}
